08cd1e9f2d8e24b425f2171071297145aaa2c3b4
bthalpin <brian.t.halpin@gmail.com>
2021-07-09 00:08:05 -0400

    finishing touch

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,15 +4,15 @@ import './ImageLinkForm.css';
 const ImageLinkForm = ({onInputChange,onSubmit,input}) => {
     return (
         <div>
-            <p className = 'f3'>
+            <p className = 'f3 white'>
                 {'This Magic Brain will detect faces in your pictures.  Give it a try.'}
             </p>
             <div className = 'center'>
                 <div className = 'form center pa4 br3 shadow-5'>
                     <input className = 'f4 pa2 w-70 center' value={input} type = 'text' onChange = {onInputChange}/>
                     <button 
-                        className = 'w-30 grow f4 link ph3 pv2 dib white bg-light-purple' 
-                        onClick = {onSubmit}>DETECT
+                        className = 'flex center w-30 grow f4 link ph3 pv2 dib white bg-gray' 
+                        onClick = {onSubmit}>Detect
                     </button>
                 </div>
             </div>
